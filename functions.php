7ed5d10e90af22e417bd63d14b525e5c31c6c57b
Irine Egorenkova <17315833+IrineEgo@users.noreply.github.com>
2018-11-15 20:12:26 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php 
+    function isRegistration() {
+            return (isset($_POST['registration']) && !empty($_POST['login']) && !empty($_POST['password']));
+    }
+    function isAuthorization() {
+            return (isset($_POST['authorization']) && !empty($_POST['login']) && !empty($_POST['password']));
+    }
+    function createPDO() {
+            $pdo = new PDO("mysql:host=localhost;dbname=iegorenkova;charset=utf8", "iegorenkova", "neto1897", [
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+            return $pdo;
+    }
+    function getSalt() {
+        return 'S76Mwq2ZwWQXo8sri';
+        //return 'Hk532falc0e84GjdF';
+    }
+    function getHashPassword($password) {
+        return md5($password . getSalt());
+    }
+?>
